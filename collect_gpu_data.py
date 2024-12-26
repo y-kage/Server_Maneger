a2b81b45a4b9d8e33d4516e6582603c1d0cb6faa
y-kage <125951749+y-kage@users.noreply.github.com>
2024-12-26 08:16:06 +0400

    Update collect_gpu_data.py

--- collect_gpu_data.py
+++ collect_gpu_data.py
@@ -1,5 +1,6 @@
 import atexit
 import json
+import os
 import threading
 import time
 from concurrent.futures import ThreadPoolExecutor
@@ -7,6 +8,7 @@ from datetime import datetime
 
 import mysql.connector
 import paramiko
+from mysql.connector import Error
 
 
 def load_servers_from_json(json_file):
@@ -18,6 +20,22 @@ servers = load_servers_from_json("server_list.json")
 
 
 def get_mysql_connection():
+    while True:
+        try:
+            connection = mysql.connector.connect(
+                host="localhost",
+                user="root",  # MySQLのユーザー名
+                password="hvrl",  # MySQLのパスワード
+                database="Server_GPU_Usage",  # データベース名
+            )
+            if connection.is_connected():
+                print("MySQL server is up and running.")
+                connection.close()
+                break
+        except Error as e:
+            print(f"Waiting for MySQL server to start... Error: {e}")
+            time.sleep(5)
+
     return mysql.connector.connect(
         host="localhost",
         user="root",  # MySQLのユーザー名
@@ -27,66 +45,69 @@ def get_mysql_connection():
 
 
 def insert_data_to_mysql(cursor, connection, result):
-    host_name = result["Name"]
-    host_ip = result["hostip"]
-    nvidia_info = result["nvidia_smi_output"]
-    timestamp = result["timestamp"]
+    try:
+        host_name = result["Name"]
+        host_ip = result["hostip"]
+        nvidia_info = result["nvidia_smi_output"]
+        timestamp = result["timestamp"]
 
-    for i in range(len(nvidia_info)):
-        _data = nvidia_info[i]
-        _data = _data.split(",")
-        for k in range(len(_data)):
-            if _data[k][0] == " ":
-                _data[k] = _data[k][1:]
+        for i in range(len(nvidia_info)):
+            _data = nvidia_info[i]
+            _data = _data.split(",")
+            for k in range(len(_data)):
+                if _data[k][0] == " ":
+                    _data[k] = _data[k][1:]
 
-            if k == 1 or k == 9:
-                pass
-            elif k == 4 or k == 5:
-                _data[k] = float(_data[k])
-                _data[k] = int(_data[k])
-            else:
-                _data[k] = int(_data[k])
-        (
-            gpu_index,
-            gpu_name,
-            fan_speed,
-            temperature,
-            power_usage,
-            power_capacity,
-            memory_usage,
-            memory_capacity,
-            gpu_utilization,
-            _,
-        ) = _data
+                if k == 1 or k == 9:
+                    pass
+                elif k == 4 or k == 5:
+                    _data[k] = float(_data[k])
+                    _data[k] = int(_data[k])
+                else:
+                    _data[k] = int(_data[k])
+            (
+                gpu_index,
+                gpu_name,
+                fan_speed,
+                temperature,
+                power_usage,
+                power_capacity,
+                memory_usage,
+                memory_capacity,
+                gpu_utilization,
+                _,
+            ) = _data
 
-        # SQLクエリを作成
-        sql = """
-            INSERT INTO gpu_usage (
-                host_name, host_ip, gpu_index, gpu_name, fan_speed, temperature,
-                power_usage, power_capacity, memory_usage, memory_capacity,
-                gpu_utilization, timestamp
-            ) VALUES (
-                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+            # SQLクエリを作成
+            sql = """
+                INSERT INTO gpu_usage (
+                    host_name, host_ip, gpu_index, gpu_name, fan_speed, temperature,
+                    power_usage, power_capacity, memory_usage, memory_capacity,
+                    gpu_utilization, timestamp
+                ) VALUES (
+                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+                )
+            """
+            data = (
+                host_name,
+                host_ip,
+                gpu_index,
+                gpu_name,
+                fan_speed,
+                temperature,
+                power_usage,
+                power_capacity,
+                memory_usage,
+                memory_capacity,
+                gpu_utilization,
+                timestamp,
             )
-        """
-        data = (
-            host_name,
-            host_ip,
-            gpu_index,
-            gpu_name,
-            fan_speed,
-            temperature,
-            power_usage,
-            power_capacity,
-            memory_usage,
-            memory_capacity,
-            gpu_utilization,
-            timestamp,
-        )
 
-        # データベースに挿入
-        cursor.execute(sql, data)
-        connection.commit()
+            # データベースに挿入
+            cursor.execute(sql, data)
+            connection.commit()
+    except:
+        print(f"Error: Cannot insert data {host_name}")
 
 
 def execute_nvidia_smi_csv(Name, hostip, username, password):
@@ -148,7 +169,7 @@ def get_all_gpu_status_csv():
 stop_event = threading.Event()
 
 
-# 定期的にデータを取得してログを残す（1時間ごと）
+# 定期的にデータを取得してログを残す
 def schedule_data_collection():
     while not stop_event.is_set():
         connection = get_mysql_connection()
@@ -165,6 +186,12 @@ def schedule_data_collection():
         connection.close()
         # time.sleep(60)
         time.sleep(1800)
+        if os.path.exists("log_app_mysql.txt"):
+            with open("log_app_mysql.txt", "w") as f:
+                pass
+        if os.path.exists("log_collect_gpu_data.txt"):
+            with open("log_collect_gpu_data.txt", "w") as f:
+                pass
 
 
 # データ収集スレッドを開始
